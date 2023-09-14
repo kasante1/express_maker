b3d01fb08b6e60b7ce3800ac9594e35be2ec2635
ds asante <114578080+dsasante1@users.noreply.github.com>
2023-09-14 21:02:24 +0000

    chore: add cli_args folder

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"express_maker/cli_args"
+	"os"
+)
+
+func main() {
+	fmt.Println("express_maker!\n Usage: <project_name>")
+	fmt.Println("")
+
+	if len(os.Args) == 1 {
+		fmt.Println("[X] enter a name for your express js project.")
+		return
+	}
+
+	// get fetch cli_arguments
+	cli_arguments := os.Args
+
+	// get cli project argument
+	cli_project_argument := cli_arguments[1]
+
+	// checked if the cli_argument supplied is a directory
+	var fileOrDirectory bool = cli_args.IsArgumentDirectory(cli_project_argument)
+
+	// if cli is a directory notify user or
+	// create project in the existing directory
+	if fileOrDirectory {
+		var dir_status error = cli_args.AlreadyExist(cli_project_argument)
+
+		if dir_status != nil {
+			fmt.Println(dir_status)
+		}
+
+	} else {
+		// if cli_project_argument is not a directory
+		// create new directory in CWD
+		cli_args.NewProjectDirectry(cli_project_argument)
+	}
+
+}
+
+
